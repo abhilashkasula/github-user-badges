703f3c3428d90325b6a4a2132690c6d64cb4ef59
Abhilash Kasula <58025056+abhilashkasula@users.noreply.github.com>
2020-07-01 15:34:02 +0530

    moved to src

--- src/gradeUser.js
+++ src/gradeUser.js
@@ -1,10 +1,11 @@
+const badges = ['None', 'Beginner', 'Beginner', 'Average', 'Average', 'Pro'];
+
 const grade = (repoLanguages) => {
   return new Promise((resolve, reject) => {
     const languages = repoLanguages.reduce((unique, languages) => {
-      Object.keys(languages).forEach(lang => unique.add(lang));
-      return unique
+      Object.keys(languages).forEach((lang) => unique.add(lang));
+      return unique;
     }, new Set());
-    const badges = ['None', 'Beginner', 'Average', 'Pro'];
     const badge = badges[languages.size] || 'Pro';
     resolve({badge, languages: [...languages]});
   });
---
+++ src/jobs.sh
@@ -0,0 +1,7 @@
+curl -X POST localhost:8000/grade/abhilashkasula
+curl -X POST localhost:8000/grade/craftybones
+curl -X POST localhost:8000/grade/naveen-kumar-vadla
+curl -X POST localhost:8000/grade/armanaaquib
+curl -X POST localhost:8000/grade/rajvadeghar
+curl -X POST localhost:8000/grade/vharidas
+curl -X POST localhost:8000/grade/blpabhishek
